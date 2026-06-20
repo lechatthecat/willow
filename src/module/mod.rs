62f3b33bb4d1a33f5aeeaa3e28f4f6afe4a831fb
lechatthecat <shu845@gmail.com>
2026-06-21 00:03:09 +0900

    Center module resolution on dependency graph

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -3,4 +3,6 @@ pub mod resolver;
 pub mod source_file;
 pub mod std_registry;
 
-pub use resolver::{ResolvedModule, resolve_imports};
+pub use module_graph::{ModuleGraph, ModuleId};
+pub use resolver::resolve_imports;
+pub use source_file::SourceFile as ResolvedModule;
